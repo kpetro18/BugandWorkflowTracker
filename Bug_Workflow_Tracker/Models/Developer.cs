32a5a20e5584ddc7a5500e4819a764ec458b4c39
kevin petroviak <kpetro18@gmail.com>
2021-03-03 14:51:47 -0600

    Created/Added member variables for ProjectManager and Developer classes

---
+++ Bug_Workflow_Tracker/Models/Developer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug_Workflow_Tracker.Models
+{
+    public class Developer
+    {
+        [Key]
+        public int Dev_Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string First_name { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string Last_Name { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Developer Production Rate")]
+        public double Dev_ProductionRate { get; set; }
+
+        //foreign keys
+    }
+}
---
+++ Bug_Workflow_Tracker/Models/ProjectManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug_Workflow_Tracker.Models
+{
+    public class ProjectManager
+    {
+        [Key]
+        public int PM_Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [ForeignKey("Developer")]
+        [Display(Name = "Developer Production Rate")]
+        public int Dev_Productionrate { get; set; }
+        public Developer? Developer { get; set; }
+
+        // add [ForeignKey("Developer")]
+    }
+}
