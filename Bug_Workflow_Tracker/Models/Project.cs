37ce1b751852e1eddd28501b8e57223cb98a2a28
kevin petroviak <kpetro18@gmail.com>
2021-03-03 15:04:22 -0600

    Created/Added member variables for Project and Ticket classes

---
+++ Bug_Workflow_Tracker/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug_Workflow_Tracker.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Project_Id { get; set; }
+
+        //add FKs
+    }
+}
---
+++ Bug_Workflow_Tracker/Models/Ticket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bug_Workflow_Tracker.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Ticket_Id { get; set; }
+
+        [Display(Name = "Ticket Type")]
+        public string TicketType { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Comment")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Date Changed")]
+        public DateTime DareChanged { get; set; }
+
+        [Display(Name = "Priority Level")]
+        public string PriorityLevel { get; set; } //auto set to high when ticket is created
+
+        [Display(Name = "Time Updated")]
+        public DateTime TimeUpdated { get; set; }
+
+        [Display(Name = "Time Completed")]
+        public DateTime TimeCompleted { get; set; }
+
+        //add FKs
+
+    }
+}
